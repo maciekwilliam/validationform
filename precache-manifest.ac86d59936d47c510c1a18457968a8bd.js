028f01348c72663c7aa425b5c6c99321c70f79b5
maciekwilliam <maciejwillem@gmail.com>
2019-03-15 18:02:08 +0100

    Updates

---
+++ precache-manifest.ac86d59936d47c510c1a18457968a8bd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42d8d449838da7f99ae3",
+    "url": "/validationform/static/js/runtime~main.47f3243e.js"
+  },
+  {
+    "revision": "fa6dad80e5d7556578c5",
+    "url": "/validationform/static/js/main.bf76766c.chunk.js"
+  },
+  {
+    "revision": "6c1856be4c8ad176c572",
+    "url": "/validationform/static/js/2.9752904f.chunk.js"
+  },
+  {
+    "revision": "fa6dad80e5d7556578c5",
+    "url": "/validationform/static/css/main.6381bc4b.chunk.css"
+  },
+  {
+    "revision": "0cb3637d12d6bbcc8acc598e17789a0a",
+    "url": "/validationform/index.html"
+  }
+];
